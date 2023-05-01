4cac090b7cb19d09396ff020ed4b421e47edb94a
Alexander.Drumev <drumev00@gmail.com>
2023-05-01 14:41:45 +0200

    Simple builder pattern I did on my own

---
+++ Builder/BuilderExercise/CodeBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace BuilderExercise
+{
+	internal class CodeBuilder
+	{
+		private StringBuilder _sb;
+		private const int _indentSpaceTimes = 2;
+
+		public CodeBuilder(string className)
+		{
+			_sb = new StringBuilder();
+
+			_sb.AppendLine($"public class {className}");
+			_sb.AppendLine("{");
+		}
+
+		public CodeBuilder AddField(string fieldName, string fieldType) 
+		{
+			_sb.AppendLine($"{new string(' ', _indentSpaceTimes)}public {fieldType} {fieldName};");
+
+			return this;
+		}
+
+		public override string ToString()
+		{
+			_sb.AppendLine("}");
+			return _sb.ToString();
+		}
+	}
+}
