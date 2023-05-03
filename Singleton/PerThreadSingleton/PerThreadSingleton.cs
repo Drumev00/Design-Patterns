82bd858b9fc92d66ee751a59df14bec400f2d613
Alexander.Drumev <drumev00@gmail.com>
2023-05-03 22:01:24 +0200

    creating an instance per thread to achieve thread safety

---
+++ Singleton/PerThreadSingleton/PerThreadSingleton.cs
@@ -0,0 +1,17 @@
+﻿namespace PerThreadSingleton
+{
+	internal sealed class PerThreadSingleton
+	{
+		// creating an instance per thread to achieve thread safety
+		private static ThreadLocal<PerThreadSingleton> _threadInstance = new ThreadLocal<PerThreadSingleton>(() => new PerThreadSingleton());
+
+		private PerThreadSingleton()
+		{
+			Id = Thread.CurrentThread.ManagedThreadId;
+		}
+
+		public int Id { get; set; }
+
+		public static PerThreadSingleton ThreadInstance => _threadInstance.Value;
+	}
+}
---
+++ Singleton/PerThreadSingleton/Program.cs
@@ -0,0 +1,13 @@
+﻿using PerThreadSingleton;
+
+var thread1 = Task.Factory.StartNew(() =>
+{
+	Console.WriteLine($"t1: {PerThreadSingleton.PerThreadSingleton.ThreadInstance.Id}");
+});
+
+var thread2 = Task.Factory.StartNew(() =>
+{
+	Console.WriteLine($"t2: {PerThreadSingleton.PerThreadSingleton.ThreadInstance.Id}");
+	Console.WriteLine($"t2: {PerThreadSingleton.PerThreadSingleton.ThreadInstance.Id}");
+});
+Task.WaitAll(thread1, thread2);
