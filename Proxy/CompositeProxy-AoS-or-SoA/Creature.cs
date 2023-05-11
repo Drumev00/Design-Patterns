0b9b4e254c6cba68aeade392b9738fb3ebf767d6
Alexander.Drumev <drumev00@gmail.com>
2023-05-11 16:04:18 +0200

    Combining composite and proxy design patterns, constructing Array of Structures/Structure of Arrays

---
+++ Proxy/CompositeProxy-AoS-or-SoA/Creature.cs
@@ -0,0 +1,52 @@
+﻿namespace CompositeProxy_AoS_or_SoA
+{
+	internal class Creature
+	{
+		public byte Age { get; set; }
+
+		public int X, Y;
+
+		internal class Creatures
+		{
+			private readonly int _size;
+			private byte[] _ages;
+			private int[] _x, _y;
+
+			public Creatures(int size)
+			{
+				_size = size;
+				_ages = new byte[size];
+				_x = new int[size];
+				_y = new int[size];
+			}
+
+
+			internal struct CreatureProxy
+			{
+				private readonly Creatures _creatures;
+
+				// can be exposed as public
+				private readonly int _index;
+
+				public CreatureProxy(Creatures creatures, int index)
+				{
+					_creatures = creatures;
+					_index = index;
+				}
+
+				// reference to age of particular creature
+				public ref byte Age => ref _creatures._ages[_index];
+				public ref int X => ref _creatures._x[_index];
+				public ref int Y => ref _creatures._y[_index];
+
+			}
+			public IEnumerator<CreatureProxy> GetEnumerator()
+			{
+				for (int pos = 0; pos < _size; ++pos)
+				{
+					yield return new CreatureProxy(this, pos);
+				}
+			}
+		}
+	}
+}
---
+++ Proxy/CompositeProxy-AoS-or-SoA/Program.cs
@@ -0,0 +1,17 @@
+﻿using CompositeProxy_AoS_or_SoA;
+using static CompositeProxy_AoS_or_SoA.Creature;
+
+// AoS
+var creatures = new Creature[100];
+foreach (var creature in creatures)
+{
+	creature.X++;
+}
+
+// SoA
+Creatures creatures2 = new Creatures(100);
+foreach (Creatures.CreatureProxy creature in creatures2)
+{
+	creature.X++;
+}
+
