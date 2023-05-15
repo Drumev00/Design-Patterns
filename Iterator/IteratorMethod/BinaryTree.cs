9d37091e76d371d4361d9d8f905a05f4a83da907
Alexander.Drumev <drumev00@gmail.com>
2023-05-15 20:11:38 +0200

    Allowing foreach without implementing IEnumerable

--- Iterator/IteratorMethod/BinaryTree.cs
+++ Iterator/IteratorMethod/BinaryTree.cs
@@ -1,5 +1,6 @@
 ﻿namespace IteratorMethod
 {
+	// NOT IEnumerable -> InOrderIterator<T> has Current and MoveNext()!
 	internal class BinaryTree<T>
 	{
 		private Node<T> _root;
@@ -9,35 +10,42 @@
 			_root = root;
 		}
 
-		public IEnumerable<Node<T>> InOrder
-		{
-			get
-			{
-				// C# 7 embedding method inside a method
-				IEnumerable<Node<T>> Traverse(Node<T> current)
-				{
-					if (current.Left != null)
-					{
-						foreach (var left in Traverse(current.Left))
-						{
-							yield return left;
-						}
-					}
-					yield return current;
-					if (current.Right != null)
-					{
-						foreach (var right in Traverse(current.Right))
-						{
-							yield return right;
-						}
-					}
-				}
+		//public IEnumerable<Node<T>> InOrder
+		//{
+		//	get
+		//	{
+		//		// C# 7 embedding method inside a method
+		//		IEnumerable<Node<T>> Traverse(Node<T> current)
+		//		{
+		//			if (current.Left != null)
+		//			{
+		//				foreach (var left in Traverse(current.Left))
+		//				{
+		//					yield return left;
+		//				}
+		//			}
+		//			yield return current;
+		//			if (current.Right != null)
+		//			{
+		//				foreach (var right in Traverse(current.Right))
+		//				{
+		//					yield return right;
+		//				}
+		//			}
+		//		}
 
-				foreach (var node in Traverse(_root))
-				{
-					yield return node;
-				}
-			}
+		//		foreach (var node in Traverse(_root))
+		//		{
+		//			yield return node;
+		//		}
+		//	}
+		//}
+
+
+		// in case in-order is the ONLY way of traversing
+		public InOrderIterator<T> GetEnumerator()
+		{
+			return new InOrderIterator<T>(_root);
 		}
 	}
 }
--- Iterator/IteratorMethod/Program.cs
+++ Iterator/IteratorMethod/Program.cs
@@ -8,4 +8,8 @@
 
 var root = new Node<int>(1, new Node<int>(2), new Node<int>(3));
 var tree = new BinaryTree<int>(root);
-Console.WriteLine(string.Join(", ", tree.InOrder.Select(x => x.Value)));
+
+foreach (var node in tree)
+{
+	Console.WriteLine(node.Value);
+}
