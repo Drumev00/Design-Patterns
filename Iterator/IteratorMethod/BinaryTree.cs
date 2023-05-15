5beb5914c8ccc30e17b1ef1a9321cd82251f6220
Alexander.Drumev <drumev00@gmail.com>
2023-05-15 19:58:51 +0200

    Traversing the same tree in a proper and more readable way

---
+++ Iterator/IteratorMethod/BinaryTree.cs
@@ -0,0 +1,43 @@
+﻿namespace IteratorMethod
+{
+	internal class BinaryTree<T>
+	{
+		private Node<T> _root;
+
+		public BinaryTree(Node<T> root)
+		{
+			_root = root;
+		}
+
+		public IEnumerable<Node<T>> InOrder
+		{
+			get
+			{
+				// C# 7 embedding method inside a method
+				IEnumerable<Node<T>> Traverse(Node<T> current)
+				{
+					if (current.Left != null)
+					{
+						foreach (var left in Traverse(current.Left))
+						{
+							yield return left;
+						}
+					}
+					yield return current;
+					if (current.Right != null)
+					{
+						foreach (var right in Traverse(current.Right))
+						{
+							yield return right;
+						}
+					}
+				}
+
+				foreach (var node in Traverse(_root))
+				{
+					yield return node;
+				}
+			}
+		}
+	}
+}
---
+++ Iterator/IteratorMethod/Program.cs
@@ -0,0 +1,11 @@
+﻿using IteratorMethod;
+
+//		1
+//	   / \
+//	  2   3
+// in-order: 213
+
+
+var root = new Node<int>(1, new Node<int>(2), new Node<int>(3));
+var tree = new BinaryTree<int>(root);
+Console.WriteLine(string.Join(", ", tree.InOrder.Select(x => x.Value)));
