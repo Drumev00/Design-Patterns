117dece98c8f91b2c3097fb429789ab15da69b90
Alexander.Drumev <drumev00@gmail.com>
2023-05-15 20:26:16 +0200

    Backing properties with an array

---
+++ Iterator/Array-BackedProperties/Creature.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+namespace Array_BackedProperties
+{
+	// i want to iterate over all properties and calculate average statistics
+	internal class Creature : IEnumerable<int>
+	{
+		private int[] stats = new int[3];
+		private const int _strength = 0;
+		// 1..2...
+
+		public int Strength
+		{
+			get
+			{
+				return stats[_strength];
+			}
+			set
+			{
+				stats[_strength] = value;
+			}
+		}
+
+		public int Agility { get; set; }
+		public int Intelligence { get; set; }
+
+		public double AverageStat => stats.Average();
+		public IEnumerator<int> GetEnumerator()
+		{
+			return stats.AsEnumerable().GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		public int this[int index]
+		{
+			get => stats[index];
+			set => stats[index] = value;
+		}
+	}
+}
