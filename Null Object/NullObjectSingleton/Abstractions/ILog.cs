4622a07366aa8aa5eb0c2818143b4ec7f1c9e1fe
Alexander.Drumev <drumev00@gmail.com>
2023-05-17 18:44:54 +0200

    Exposing a singleton instance of NullLog via a static interface member, allowing me to feed null objects when dependencies require them

---
+++ Null Object/NullObjectSingleton/Abstractions/ILog.cs
@@ -0,0 +1,26 @@
+﻿namespace NullObjectSingleton.Abstractions
+{
+	internal interface ILog
+	{
+		void Warn();
+
+		public static ILog Null => NullLog.Instance;
+
+		private sealed class NullLog : ILog
+		{
+			private static Lazy<NullLog> _instance = new Lazy<NullLog>(() => new NullLog());
+
+			public static ILog Instance => _instance.Value;
+
+			private NullLog()
+			{
+
+			}
+
+			public void Warn()
+			{
+
+			}
+		}
+	}
+}
