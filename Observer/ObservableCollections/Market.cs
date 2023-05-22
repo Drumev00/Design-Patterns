76571ff981f51773cb8b959175bf4560fa3be2cb
Alexander.Drumev <drumev00@gmail.com>
2023-05-22 18:01:27 +0200

    Notifying when a collection changes with BindingList

---
+++ Observer/ObservableCollections/Market.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace ObservableCollections
+{
+	internal class Market
+	{
+		// has a problem - for example I don't know when I ran out of items (unless create separate event for it)
+		public readonly BindingList<float> Prices = new();
+
+		public void AddPrice(float price)
+		{
+			Prices.Add(price);
+		}
+	}
+}
---
+++ Observer/ObservableCollections/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using ObservableCollections;
+
+var market = new Market();
+market.Prices.ListChanged += (sender, args) =>
+{
+	if (args.ListChangedType == ListChangedType.ItemAdded)
+	{
+		float price = ((BindingList<float>)sender)[args.NewIndex];
+		Console.WriteLine($"BindingList got a price of {price}");
+	}
+};
+market.AddPrice(123);
