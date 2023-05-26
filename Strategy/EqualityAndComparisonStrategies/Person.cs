f64413abdad3237c83d0c01df9e06a4646fa4178
Alexander.Drumev <drumev00@gmail.com>
2023-05-26 16:36:32 +0200

    Using default and custom strategy to compare and sort objects

---
+++ Strategy/EqualityAndComparisonStrategies/Person.cs
@@ -0,0 +1,73 @@
+﻿namespace EqualityAndComparisonStrategies
+{
+	// can use default or custom strategy
+	internal class Person : IComparable<Person>, IComparable
+	{
+		public Person(int id, string name, int age)
+		{
+			Id = id;
+			Name = name;
+			Age = age;
+		}
+
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public int Age { get; set; }
+
+		// default strategy
+		public int CompareTo(Person? other)
+		{
+			if (ReferenceEquals(this, other)) return 0;
+			if (ReferenceEquals(null, other)) return 1;
+			return Id.CompareTo(other.Id);
+		}
+
+		public int CompareTo(object? obj)
+		{
+			if (ReferenceEquals(null, obj)) return 1;
+			if (ReferenceEquals(this, obj)) return 0;
+			return obj is Person other ? CompareTo(other) : throw new ArgumentException($"Object must be of type {nameof(Person)}");
+		}
+
+		public static bool operator <(Person? left, Person? right)
+		{
+			return Comparer<Person>.Default.Compare(left, right) < 0;
+		}
+
+		public static bool operator >(Person? left, Person? right)
+		{
+			return Comparer<Person>.Default.Compare(left, right) > 0;
+		}
+
+		public static bool operator <=(Person? left, Person? right)
+		{
+			return Comparer<Person>.Default.Compare(left, right) <= 0;
+		}
+
+		public static bool operator >=(Person? left, Person? right)
+		{
+			return Comparer<Person>.Default.Compare(left, right) >= 0;
+		}
+
+		// custom strategy
+		private sealed class NameRelationalComparer : IComparer<Person>
+		{
+			public int Compare(Person x, Person y)
+			{
+				if (ReferenceEquals(x, y)) return 0;
+				if (ReferenceEquals(null, y)) return 1;
+				if (ReferenceEquals(null, x)) return -1;
+				return string.Compare(x.Name, y.Name, StringComparison.Ordinal);
+			}
+		}
+
+		public static IComparer<Person> NameComparer { get; } = new NameRelationalComparer();
+
+		public override string ToString()
+		{
+			return $"{nameof(Id)}: {Id}, {nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+		}
+	}
+}
---
+++ Strategy/EqualityAndComparisonStrategies/Program.cs
@@ -0,0 +1,30 @@
+﻿using EqualityAndComparisonStrategies;
+
+var people = new List<Person>()
+{
+	new Person(2, "Bryan", 21),
+	new Person(204, "Aaron", 29),
+	new Person(42, "John", 41),
+	new Person(22, "Ron", 33),
+};
+
+Console.WriteLine("By Id (default)");
+// after implementing the default strategy (comparing objects, using relational operators) - this will sort by Id
+people.Sort();
+foreach (var person in people)
+{
+	Console.WriteLine(person);
+}
+
+// sorts by name
+// people.Sort((x, y) => x.Name.CompareTo(y.Name));
+
+Console.WriteLine("By name (custom)");
+// sorts by name as well
+people.Sort(Person.NameComparer);
+foreach (var person in people)
+{
+	Console.WriteLine(person);
+}
+
+
