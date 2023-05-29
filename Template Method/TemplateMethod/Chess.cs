326586d80b77309beae5327f409fb87c852ce6c8
Alexander.Drumev <drumev00@gmail.com>
2023-05-29 17:06:01 +0200

    Template Method - building a skeleton for an algorithm in a base class and adding concreteness in derived classes

---
+++ Template Method/TemplateMethod/Chess.cs
@@ -0,0 +1,27 @@
+﻿namespace TemplateMethod
+{
+	internal class Chess : Game
+	{
+		private int _turn = 1;
+		private int _maxTurns = 10;
+
+		public Chess() : base(2)
+		{
+		}
+
+		protected override bool HaveWinner => _turn == _maxTurns;
+
+		protected override int WinningPlayer => currentPlayer;
+
+		protected override void Start()
+		{
+			Console.WriteLine($"Starting a game of chess with {numberOfPlayers} players.");
+		}
+
+		protected override void TakeTurn()
+		{
+			Console.WriteLine($"Turn {_turn++} taken by player {currentPlayer}.");
+			currentPlayer = (currentPlayer + 1) % numberOfPlayers;
+		}
+	}
+}
---
+++ Template Method/TemplateMethod/Game.cs
@@ -0,0 +1,30 @@
+﻿namespace TemplateMethod
+{
+	internal abstract class Game
+	{
+		protected int currentPlayer;
+		protected readonly int numberOfPlayers;
+		protected abstract bool HaveWinner { get; }
+		protected abstract int WinningPlayer { get; }
+
+		public Game(int numberOfPlayers)
+		{
+			this.numberOfPlayers = numberOfPlayers;
+		}
+
+		// template method
+		public void Run()
+		{
+			Start();
+			while (!HaveWinner)
+			{
+				TakeTurn();
+			}
+			Console.WriteLine($"Player {WinningPlayer} wins.");
+		}
+
+		protected abstract void Start();
+		protected abstract void TakeTurn();
+
+	}
+}
