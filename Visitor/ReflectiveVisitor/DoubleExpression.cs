a59d68f4fed42a2358db6d1174406dc54bdf967c
Alexander.Drumev <drumev00@gmail.com>
2023-05-30 16:28:38 +0200

    Having one static method responsible for printing output, this time in a reflective way.

---
+++ Visitor/ReflectiveVisitor/DoubleExpression.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace ReflectiveVisitor
+{
+	internal class DoubleExpression : Expression
+	{
+		public double Value { get; }
+
+		public DoubleExpression(double value)
+		{
+			Value = value;
+		}
+
+	}
+}
---
+++ Visitor/ReflectiveVisitor/Program.cs
@@ -0,0 +1,8 @@
+﻿using ReflectiveVisitor;
+using System.Text;
+
+var expr = new AdditionExpression(new DoubleExpression(1), new AdditionExpression(
+	new DoubleExpression(2), new DoubleExpression(3)));
+var sb = new StringBuilder();
+ExpressionPrinter.Print(expr, sb);
+Console.WriteLine(sb);
